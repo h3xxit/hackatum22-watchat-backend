5690e1460ebad048d56f71c02680e3be76a8f3e5
Arne <ge47com@mytum.de>
2022-11-19 16:25:22 +0100

    changed graph.py to fit our database structure

--- graphComputation/graph.py
+++ graphComputation/graph.py
@@ -1,54 +1,65 @@
 import psycopg2
 from sortedcontainers import SortedDict
 
-DBNAME = "streaming_data"
+DBNAME = "streaming-data"
 DBUSER = "arne"
 GRAPHDEPTH = 10
-
+WEIGHTS = {
+      'anime': 0.4,
+      'car': 0.5,
+      'battle': 0.8,
+      'love': 1.0,
+      'comedy': 0.95,
+      'happy': 0.9,
+      'sad': 0.85,
+      'police': 0.35,
+      'superhero': 0.7,
+      'dystopia': 0.25,
+      'popularity': 0.01
+      }
 
 class Node:
-  DIM = 10
-  WEIGHT = []
 
-  def __init__(self, vector, idNumber):
-    self.vec = vector
+  def __init__(self, tags, idNumber):
+    self.tags = tags
     self.neighbours = SortedDict()
     self.idNumber = idNumber
 
   def dist(self, node):
-    res = 0;
-    for i in range(0,Node.DIM):
-      res += pos(self.vector[i] - node.vector[i],2) * Node.WEIGHT[i] # weighted euclidean distance
+    res = 0
+    for name, match in self.tags.items():
+      res += pow(match - node.tags[name],2) * WEIGHTS[name] # weighted euclidean distance
     return res
 
   def updateNeighbours(self, node):
-    self.neighbours.update([(self.dist(self, node),node)])
+    self.neighbours.update([(self.dist(node),node)])
     if (len(self.neighbours) > GRAPHDEPTH):
       self.neighbours.popitem()
 
 def getDatabaseConnection():
-  return psycopg2.connect("dbname={dbname} user={user}".format(name=DBNAME, user=DBUSER))
+  return psycopg2.connect(f"dbname={DBNAME} user={DBUSER}")
 
-def insertNeighbours(node, cur):
+def insertNeighbours(node, cur, con):
   for nxt in node.neighbours.values():
-    cur.execute("""INSERT INTO Edges (startId, endId) VALUES (%s, %s)
+    cur.execute("""INSERT INTO edges_test (start_id, end_id) VALUES (%s, %s)
         """, (node.idNumber, nxt.idNumber))
+    con.commit()
 
-def getNodes(cur):
+def getNodes(cur, con):
   nodes = []
-  cur.execute("SELECT id FROM Movies")
+  cur.execute("SELECT DISTINCT tmdb_id FROM graph_data_test")
   ids = cur.fetchall()
   for idNumber in ids:
-    cur.execute("SELECT * FROM tags WHERE id = %s", idNumber)
-    tags = [x[1:] for x in cur.fetchall()]
-    tags.sort()
-    aktNode = Node([x[2] for x in tags], idNumber)
+    cur.execute("SELECT name, match FROM graph_data_test WHERE tmdb_id = %s", idNumber)
+    tag_dict = dict(cur.fetchall())
+
+    aktNode = Node(tag_dict, idNumber)
     for node in nodes:
       node.updateNeighbours(aktNode)
       aktNode.updateNeighbours(node)
     nodes.append(aktNode)
   for node in nodes:
-    insertNeighbours(node, cur)
+    insertNeighbours(node, cur, con)
 
 
 def cleanUp(cur, con):
@@ -59,5 +70,5 @@ def cleanUp(cur, con):
 
 con = getDatabaseConnection()
 cur = con.cursor()
-getNodes(cur)
+getNodes(cur, con)
 cleanUp(cur, con)
