ba986d9f22ecd0cf4185172aab6ad19d3238057d
Razvan Radulescu <43811028+h3xxit@users.noreply.github.com>
2022-11-19 03:15:37 +0100

    Added annotations

--- watchat/src/main/java/com/hackatum/watchat/repository/inMemRepository/InMemMovieRepository.java
+++ watchat/src/main/java/com/hackatum/watchat/repository/inMemRepository/InMemMovieRepository.java
@@ -1,6 +1,8 @@
 package com.hackatum.watchat.repository.inMemRepository;
 
 import com.hackatum.watchat.entities.Movie;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class InMemMovieRepository extends InMemRepository<Movie, String>{
 }
--- watchat/src/main/java/com/hackatum/watchat/service/BartLargeMnliClassifierService.java
+++ watchat/src/main/java/com/hackatum/watchat/service/BartLargeMnliClassifierService.java
@@ -1,9 +1,11 @@
 package com.hackatum.watchat.service;
 
 import com.hackatum.watchat.entities.Tag;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class BartLargeMnliClassifierService implements ClassifierService{
     @Override
     public List<Tag> classify(String text) {
