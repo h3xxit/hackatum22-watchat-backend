a0e4dc3e5530fb2421b02b91c34f67f5dca63e06
Razvan Radulescu <43811028+h3xxit@users.noreply.github.com>
2022-11-19 17:16:07 +0100

    config for heroku

--- watchat/src/main/java/com/hackatum/watchat/config/CustomCorsConfiguration.java
+++ watchat/src/main/java/com/hackatum/watchat/config/CustomCorsConfiguration.java
@@ -1,14 +1,13 @@
 package com.hackatum.watchat.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 public class CustomCorsConfiguration {
 
-    private final String corsOriginPattern = "localhost:62729";
+    private final String corsOriginPattern = "*";
 
     @Bean
     public CorsFilter corsFilter()
