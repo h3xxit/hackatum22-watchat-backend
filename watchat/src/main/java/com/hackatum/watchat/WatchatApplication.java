d3aaf7e5673aef04e5171bf1a3fe974f66e02a1d
Razvan Radulescu <43811028+h3xxit@users.noreply.github.com>
2022-11-19 02:06:31 +0100

    initial commit

---
+++ watchat/src/main/java/com/hackatum/watchat/WatchatApplication.java
@@ -0,0 +1,13 @@
+package com.hackatum.watchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchatApplication.class, args);
+	}
+
+}
---
+++ watchat/src/test/java/com/hackatum/watchat/WatchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackatum.watchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
